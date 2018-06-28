fef4e514264d6f233e7c2cf39efbf7a1a40ca6d1
Virtum <pawel.filocha@gmail.com>
2018-06-28 07:14:04 +0200

    Add new methods to RequestHandler interface

--- src/main/java/com/filocha/messaging/server/RequestHandler.java
+++ src/main/java/com/filocha/messaging/server/RequestHandler.java
@@ -3,4 +3,8 @@ package com.filocha.messaging.server;
 public interface RequestHandler<TRequest, TResponse> {
 
     TResponse onMessage(TRequest request);
+
+    TRequest getRequestClass(TRequest requestClass);
+
+    TResponse getResponseClass(TResponse responseClass);
 }
